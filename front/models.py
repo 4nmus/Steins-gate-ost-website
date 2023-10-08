7efb9fb9025572aadf37389e9328d4cd8a38d694
4nmus <andr_cyp@proton.me>
2023-10-08 22:09:00 +0300

    Model, Views, etc. added. Release in 3 days

--- front/models.py
+++ front/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Song(models.Model):
+    title = models.CharField('Name of the song',unique=True, max_length=50)
+    date = models.DateTimeField('date', default=timezone.now)
+    song = models.FileField(upload_to="files")
+
--- front/views.py
+++ front/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from django.shortcuts import render
+from .models import Song
 # Create your views here.
+
+
+def HomeView(request):
+    songs = Song.objects.all()
+    num = songs.count()
+    context = {'forums': songs, 'count': num,}
+    return render(request, 'Main_Forum/Home.html', context)
