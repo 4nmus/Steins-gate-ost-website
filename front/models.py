8ac0ae9643dfc6e243669614bf39965acb4129ee
4nmus <andr_cyp@proton.me>
2023-10-13 21:24:20 +0300

    some improvements in structure

--- front/models.py
+++ front/models.py
@@ -3,8 +3,9 @@ from django.utils import timezone
 
 # Create your models here.
 
-class Song(models.Model):
+class Songs(models.Model):
     title = models.CharField('Name of the song',unique=True, max_length=50)
     date = models.DateTimeField('date', default=timezone.now)
+    image = models.ImageField(upload_to="files")
     song = models.FileField(upload_to="files")
 
--- front/views.py
+++ front/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.shortcuts import render
-from .models import Song
+from .models import Songs
 # Create your views here.
 
 
-def HomeView(request):
-    songs = Song.objects.all()
-    num = songs.count()
-    context = {'forums': songs, 'count': num,}
-    return render(request, 'front/home.html', context)
+class HomeView(ListView):
+    model = Songs
+    template_name = 'front/base.html'
+    context_object_name = 'threads'
+    paginate_by = 4
+    ordering = ['date']
+    def get_context_data(self, **kwargs):
+        ctx = super(HomeView, self).get_context_data(**kwargs)
+        ctx['songs'] = Songs.objects.all()
+
+        return ctx
+class SongView(ListView):
+    model = Songs
+    template_name = "front/base.html"
+    context_object_name = 'songs'
+
+    def get_context_data(self, **kwargs):
+        ctx = super(SongView, self).get_context_data(**kwargs)
+        ctx['song'] = Songs.objects.get(pk=self.kwargs['pk'])
+        return ctx
